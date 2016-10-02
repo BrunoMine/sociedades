79c3b77ab13ed8b23be11a0bd024f29f9845dfe6
BrunoMine <borgesdossantosbruno@gmail.com>
2016-10-02 17:27:59 -0300

    Remoção do item do inventario criativo

--- sunos/nodes.lua
+++ sunos/nodes.lua
@@ -21,7 +21,7 @@ minetest.register_node("sunos:bau", {
 	tiles = {"default_chest_top.png^sunos_bau_topo.png", "default_chest_top.png", "default_chest_side.png^sunos_bau_lado.png",
 		"default_chest_side.png^sunos_bau_lado.png", "default_chest_side.png^sunos_bau_lado.png", "default_chest_lock.png^sunos_bau_frente.png"},
 	paramtype2 = "facedir",
-	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, not_in_creative_inventory=1},
 	legacy_facedir_simple = true,
 	is_ground_content = false,
 	sounds = default.node_sound_wood_defaults(),
@@ -40,7 +40,7 @@ minetest.register_node("sunos:fundamento", {
 	tiles = {"default_tree_top.png^sunos_fundamento.png", "default_tree_top.png", "default_tree.png"},
 	paramtype2 = "facedir",
 	is_ground_content = false,
-	groups = {choppy = 2, oddly_breakable_by_hand = 1},
+	groups = {choppy = 2, oddly_breakable_by_hand = 1, not_in_creative_inventory=1},
 	sounds = default.node_sound_wood_defaults(),
 	drop = "default:tree",
 	

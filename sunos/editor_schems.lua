dd95c3118889979d8cea5b7fbbbee2f55f37f1e3
BrunoMine <borgesdossantosbruno@gmail.com>
2017-02-18 23:39:52 -0300

    Finalizado editor de estruturas dos sunos

--- sunos/editor_schems.lua
+++ sunos/editor_schems.lua
@@ -18,8 +18,8 @@ local lista_de_tipos = {}
 local lista_de_tipos_i = {}
 
 -- Lista de larguras
-local lista_de_larguras = {"3", "5", "7", "9", "13", "15"}
-local lista_de_larguras_i = {["3"]=1, ["5"]=2, ["7"]=3, ["9"]=4, ["13"]=5, ["15"]=6}
+local lista_de_larguras = {"3", "5", "7", "9", "13"}
+local lista_de_larguras_i = {["3"]=1, ["5"]=2, ["7"]=3, ["9"]=4, ["13"]=5}
 
 -- Acessar formspec
 local acessar_menu = function(name)
@@ -82,10 +82,9 @@ local acessar_menu = function(name)
 		.."dropdown[0,3;10,1;estrutura;"..list_estruturas_strings..";"..escolha_estrutura.."]"
 		.."button_exit[0,3.8;3,1;carregar;Carregar]"
 		.."button_exit[3,3.8;3,1;salvar;Salvar]"
-		.."button_exit[6,3.8;3,1;deletar;Deletar]"
 		
 		.."label[0,5.2;Criar nova estrutura]"
-		.."field[0.3,6.3;8,1;nome_novo;Nome da estrutura;]"
+		.."field[0.3,6.3;8,1;novo_nome;Nome da estrutura;]"
 		.."label[8,5.5;Largura]"
 		.."dropdown[8,6;2,1;nova_largura;3,5,7,9,11,13;]"
 		.."button_exit[0,6.8;6,1;criar;Salvar nova estrutura]"
@@ -103,6 +102,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		
 		-- Carregar estrutura
 		if fields.carregar then
+		
 			-- Verificar tipo
 			local tipo = minetest.get_meta(acessos[name].pos):get_string("tipo_nome")
 			-- Verificar estrutura e largura 
@@ -120,13 +120,93 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 				largura = tonumber(vv[1])			
 			end
 			
+			-- Monta a base para colocar a estrutura
+			minetest.place_schematic(
+				{x=pos.x+1, y=pos.y, z=pos.z-7}, 
+				minetest.get_modpath("sunos").."/schems_basicos/base_editor.mts", 
+				0, nil, true
+			)
+			
 			-- Verificar estrutura
+			local dist = ((largura-1)/2)
 			-- Pegar nome do caminho do arquivo
-			
 			local arq = "/"..tipo.."/"..estrutura.."."..largura..".mts"
-			minetest.chat_send_all(dump(arq))
-			minetest.place_schematic({x=pos.x+2, y=pos.y, z=pos.z}, minetest.get_modpath("sunos").."/schems/"..arq, 0, nil, true)
+			minetest.place_schematic({x=pos.x+8-dist, y=pos.y, z=pos.z-dist}, minetest.get_modpath("sunos").."/schems/"..arq, 0, nil, true)
 			
+			minetest.chat_send_player(name, "Estrutura carregada.")
+			return
+		end
+		
+		if fields.salvar then
+		
+			-- Verificar tipo
+			local tipo = minetest.get_meta(acessos[name].pos):get_string("tipo_nome")
+			-- Verificar estrutura e largura 
+			local estrutura = minetest.get_meta(acessos[name].pos):get_string("estrutura")
+			if tipo == "" or estrutura == "" then 
+				minetest.chat_send_player(name, "Defina o tipo e estrutura desejada")
+				return
+			end
+			-- Separar largura
+			local largura = ""
+			do
+				local v = string.split(estrutura, " (")
+				estrutura = v[1]
+				local vv = string.split(v[2], ")")
+				largura = tonumber(vv[1])			
+			end
+			
+			-- Pegar nome do caminho do arquivo
+			local arq = "/"..tipo.."/"..estrutura.."."..largura..".mts"
+			
+			-- Remove o antigo arquivo schematic
+			if os.remove(minetest.get_modpath("sunos").."/schems/"..arq) == nil then
+				minetest.chat_send_player(name, "Falha ao remover arquivo antigo.")
+				return
+			end
+			
+			-- Criar novo arquivo schematic
+			local dist = ((largura-1)/2)
+			minetest.create_schematic(
+				{x=pos.x+8-dist, y=pos.y, z=pos.z-dist}, 
+				{x=pos.x+8+dist, y=pos.y+14, z=pos.z+dist}, {}, 
+				minetest.get_modpath("sunos").."/schems/"..arq
+			)
+			
+			-- Avisa o bug do minetest
+			minetest.chat_send_player(name, "Estrutura foi salva. Mas precisa reiniciar o minetest para carregar ela novamente.")
+			return
+		end
+		
+		if fields.criar then
+			
+			-- Verificar nome da nova estrutura
+			if fields.novo_nome == "" then 
+				minetest.chat_send_player(name, "Defina um nome da estrutura.")
+				return
+			end
+			
+			-- Verificar tipo
+			local tipo = minetest.get_meta(acessos[name].pos):get_string("tipo_nome")
+			if tipo == "" then
+				minetest.chat_send_player(name, "Nenhum tipo definido.")
+				return
+			end
+			
+			local largura = tonumber(fields.nova_largura)
+			
+			-- Pegar nome do caminho do arquivo
+			local arq = "/"..tipo.."/"..fields.novo_nome.."."..largura..".mts"
+			
+			-- Criar novo arquivo schematic
+			local dist = ((largura-1)/2)
+			minetest.create_schematic(
+				{x=pos.x+8-dist, y=pos.y, z=pos.z-dist}, 
+				{x=pos.x+8+dist, y=pos.y+14, z=pos.z+dist}, {}, 
+				minetest.get_modpath("sunos").."/schems/"..arq
+			)
+			
+			minetest.chat_send_player(name, "Estrutura nova foi salva.")
 			return
 		end
 		
